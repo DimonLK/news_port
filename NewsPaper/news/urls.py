a98831ec8f6fa0418c44232d004258eca0956435
DimonLK <145279864+DimonLK@users.noreply.github.com>
2024-06-14 17:28:00 +0300

    Update urls.py

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -5,7 +5,4 @@ from .views import *
 urlpatterns = [
    path('', PostList.as_view(), name='post-list'),
    path('<int:pk>/', PostDetail.as_view(), name='post-detail'),
-   # path('<int:pk>/', PostForm.as_view(), name='post-form'),
-   # path('<int:pk>/', PostCreate.as_view(), name='post-create'),
-   # path('<int:pk>/', PostUpdate.as_view(), name='post-update')
 ]
